d6a5ed2e86fe1974f94a64ab029f6cc927dbe6f0
Sumit Kumar <54541071+ssumit555@users.noreply.github.com>
2022-04-02 18:16:05 +0530

    Initialize Newsite

---
+++ main.py
@@ -0,0 +1,60 @@
+# (c) @AbirHasan2005 | Thomas Shelby
+# This is Telegram Messages Forwarder UserBot!
+# Use this at your own risk. I will not be responsible for any kind of issue while using this!
+
+# Re-edited by Mrvishal2k2 as per I need 
+# Don't use  this (It's your own risk 🙄🤦😂)
+
+import time
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, UserDeactivatedBan
+from configs import Config
+
+User = Client(session_name=Config.STRING_SESSION, api_hash=Config.API_HASH, api_id=Config.API_ID)
+
+async def kanger(msg):
+    await msg.edit(text="Forwarding Now ...")
+    total_files_done = 0
+    async for message in User.iter_history(chat_id=int(Config.FORWARD_FROM_CHAT_ID),offset_id=13529,limit=9400, reverse=True):
+#        media = message.document or message.video or message.audio or message.photo 
+        media = message.document or message.video
+        if media:
+            await asyncio.sleep(Config.SLEEP_TIME)
+            try:
+                 total_files_done = total_files_done + 1
+                 await message.copy(int(Config.FORWARD_TO_CHAT_ID))
+            except FloodWait as e:
+                print(f"#FloodWait: Stopping Forwarder for `{e.x}s`!")
+                await asyncio.sleep(e.x)
+                pass
+            except UserDeactivatedBan:
+                print("Congratulations!\nYour Account Banned Successfully!\nI already told you use a Fake Account. Hope you remember.")
+                break
+            except Exception as err:
+                await User.send_message(chat_id=Config.OWNER, text=f"#ERROR: `{err}`")
+        else:
+            pass
+    await msg.edit(text=f"Channel Files Successfully Kanged! \nTotal Kanged = {total_files_done} \n\n©️ Robber")
+    await User.send_message(chat_id=Config.OWNER, text=f"Channel Files Successfully Kanged! \nTotal Kanged = {total_files_done} \n\n©️ Robber")
+
+
+@User.on_message((filters.text) & ~filters.edited)
+async def main(client, message):
+    # Checks
+    if message.text == "!start" and (message.from_user.id == int(Config.USER_ID)):
+        await message.edit(text="Hi, Myself!\nThis is a Forwarder Userbot by @AbirHasan2005", parse_mode="Markdown",
+                           disable_web_page_preview=True)
+    elif message.text == "!help" and (message.from_user.id == int(Config.USER_ID)):
+        await message.edit(
+            text="This UserBot can forward messages from any Chat to any other Chat also you can kang all messages from one Chat to another Chat.\n\n👨🏻‍💻 **Commands:**\n• `!start`\n• `!help`\n• `!kang`\n\n©️ **Developer:** @AbirHasan2005\n👥 **Support Group:** [【★ʟя★】](https://t.me/linux_repo)",
+            parse_mode="Markdown", disable_web_page_preview=True)
+    elif message.text.startswith("!kang") and (message.from_user.id == int(Config.USER_ID)):
+        editable = await message.edit(
+            text=f"Trying to Get All Messages from `{str(Config.FORWARD_FROM_CHAT_ID)}` and Forwarding to `{str(Config.FORWARD_TO_CHAT_ID)}` ...",
+            parse_mode="Markdown", disable_web_page_preview=True)
+        await asyncio.sleep(5)
+        await kanger(editable)
+        await User.send_message(chat_id=Config.OWNER, text=f"Channel Files Successfully Kanged! \nAll clear  \n\n©️ Robber")
+
+User.run()
